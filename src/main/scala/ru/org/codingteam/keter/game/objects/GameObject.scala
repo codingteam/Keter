ca82dbcba175dac77dd5ed9f7fd836b5c543a9bb
Alexander Konovalov <akon32@rambler.ru>
2014-11-04 11:09:42 +0300

    Old GameObject replaced with new definition.

--- src/main/scala/ru/org/codingteam/keter/game/objects/GameObject.scala
+++ src/main/scala/ru/org/codingteam/keter/game/objects/GameObject.scala
@@ -1,8 +1,28 @@
 package ru.org.codingteam.keter.game.objects
 
-abstract class GameObject {
+import ru.org.codingteam.keter.map.{ObjectPosition, UniverseSnapshot}
 
-  def name: String
-  def tile: String
+import scala.concurrent.Future
 
+trait GameObject extends ActorLike {
+  def owner: ActorId
+
+  def actionsList: Seq[ObjectAction]
 }
+
+trait ObjectAction {
+  def description: String
+}
+
+trait GenericObjectAction extends ObjectAction {
+  def perform(universe: UniverseSnapshot): Future[UniverseSnapshot]
+}
+
+trait ObjectActionToPosition extends ObjectAction {
+  def perform(universe: UniverseSnapshot, to: ObjectPosition): Future[UniverseSnapshot]
+}
+
+trait ObjectActionToActor extends ObjectAction {
+  def perform(universe: UniverseSnapshot, id: ActorId): Future[UniverseSnapshot]
+}
+
